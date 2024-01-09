5758f0b4836906e775c1b6ace5c1dd5e892ebd24
Marlon López <malm.gm@gmail.com>
2024-01-09 13:39:39 -0500

    feat(mobile-app): added book detail domain model

---
+++ apps/mobile-app/src/main/kotlin/dev/marlonlom/demos/bookbar/domain/books/model.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package dev.marlonlom.demos.bookbar.domain.books
+
+import dev.marlonlom.demos.bookbar.BuildConfig
+
+/**
+ * Books list item domain data class.
+ *
+ * @author marlonlom
+ *
+ * @property title Book title.
+ * @property isbn13 Book id as isb13.
+ * @property price Book price as text.
+ * @property image Book image url.
+ */
+data class BooksListItem(
+  val isbn13: String,
+  val title: String,
+  val price: String,
+  val image: String,
+)
+
+/**
+ * Book detail domain data class.
+ *
+ * @author marlonlom
+ *
+ * @property isbn13 Book id as isb13.
+ * @property isbn10 Book id as isb10.
+ * @property title Book title.
+ * @property subtitle Book subtitle.
+ * @property authors Book author names.
+ * @property publisher Book publisher name.
+ * @property language Book language name.
+ * @property pages Book total pages.
+ * @property year Book published year.
+ * @property rating Book rating value.
+ * @property desc Book description.
+ * @property price Book price as text.
+ * @property image Book image url.
+ * @property url Book url.
+ */
+data class BookDetailItem(
+  val isbn13: String,
+  val isbn10: String,
+  val title: String,
+  val subtitle: String,
+  val authors: String,
+  val publisher: String,
+  val language: String,
+  val pages: String,
+  val year: String,
+  val rating: String,
+  val desc: String,
+  val price: String,
+  val image: String,
+  val url: String
+) {
+  /** Book url for buying purposes. */
+  val buyUrl: String get() = "${BuildConfig.ITBOOKSTORE_BUY_URL}/$isbn13"
+}
