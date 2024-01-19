e19968e702c9dfbd00c46f6d25188db1b275bcf2
Marlon López <malm.gm@gmail.com>
2024-01-19 18:46:52 -0500

    feat(mobile-app): updated ui for favorite books route

--- apps/mobile-app/src/main/kotlin/dev/marlonlom/demos/bookbar/ui/features/books_favorite/FavoriteBooksRoute.kt
+++ apps/mobile-app/src/main/kotlin/dev/marlonlom/demos/bookbar/ui/features/books_favorite/FavoriteBooksRoute.kt
@@ -7,7 +7,6 @@ package dev.marlonlom.demos.bookbar.ui.features.books_favorite
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,7 +17,6 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dev.marlonlom.demos.bookbar.R
@@ -33,6 +31,7 @@ import dev.marlonlom.demos.bookbar.ui.main.BookbarAppState
  *
  * @param appState App ui state.
  * @param onBookItemClicked Action for Book item clicked.
+ * @param onRemoveFavoriteIconClicked Action for favorite book button clicked.
  */
 @Composable
 fun FavoriteBooksRoute(
@@ -46,64 +45,61 @@ fun FavoriteBooksRoute(
     else -> 20.dp
   }
 
-  Column(
+  LazyVerticalGrid(
     modifier = Modifier
       .fillMaxSize()
       .padding(horizontal = contentHorizontalPadding)
       .background(MaterialTheme.colorScheme.background),
-    horizontalAlignment = Alignment.CenterHorizontally
-  ) {
-    LazyVerticalGrid(
-      columns = GridCells.Fixed(1),
-      horizontalArrangement = Arrangement.spacedBy(20.dp),
-      content = {
-        item(span = { GridItemSpan(maxLineSpan) }) {
-          WelcomeTitle(appState = appState)
-        }
-
-        item(span = { GridItemSpan(maxLineSpan) }) {
-          HeadlineTitle(
-            appState = appState,
-            headlineTextRes = R.string.title_bottom_favorites
-          )
-        }
-
-        when (appState.favoriteBooksList) {
-          FavoriteBooksUiState.Empty -> {
-            item {
-              Text(text = "Empty results :(")
-            }
-
-          }
-
-          FavoriteBooksUiState.Loading -> {
-            item {
-              Text(text = "Loading")
-            }
-          }
-
-          is FavoriteBooksUiState.Success -> {
-            val favoriteBooksCount = appState.favoriteBooksList.books.size
-            items(favoriteBooksCount) { bookIndex ->
-              ClickableFavoriteBookItem(
-                bookIndex = bookIndex,
-                bookItem = appState.favoriteBooksList.books[bookIndex],
-                onBookItemClicked = onBookItemClicked,
-                onRemoveFavoriteIconClicked = onRemoveFavoriteIconClicked
-              )
-              if ((bookIndex > 0).and(bookIndex < favoriteBooksCount)) {
-                Divider(
-                  modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp)
-                )
-              }
-            }
-          }
-
-        }
+    columns = GridCells.Fixed(1),
+    horizontalArrangement = Arrangement.spacedBy(20.dp),
+    content = {
+      item(span = { GridItemSpan(maxLineSpan) }) {
+        WelcomeTitle(appState = appState)
       }
-    )
-  }
+
+      item(span = { GridItemSpan(maxLineSpan) }) {
+        HeadlineTitle(
+          appState = appState,
+          headlineTextRes = R.string.title_bottom_favorites
+        )
+      }
+
+      when (appState.favoriteBooksList) {
+        FavoriteBooksUiState.Empty -> {
+          item {
+            Text(text = "Empty results :(")
+          }
+
+        }
+
+        FavoriteBooksUiState.Loading -> {
+          item {
+            Text(text = "Loading")
+          }
+        }
+
+        is FavoriteBooksUiState.Success -> {
+          val favoriteBooksCount = appState.favoriteBooksList.books.size
+          items(favoriteBooksCount) { bookIndex ->
+            ClickableFavoriteBookItem(
+              bookIndex = bookIndex,
+              bookItem = appState.favoriteBooksList.books[bookIndex],
+              onBookItemClicked = onBookItemClicked,
+              onRemoveFavoriteIconClicked = onRemoveFavoriteIconClicked
+            )
+            if ((bookIndex > 0).and(bookIndex < favoriteBooksCount)) {
+              Divider(
+                modifier = Modifier
+                  .fillMaxWidth()
+                  .padding(vertical = 10.dp)
+              )
+            }
+          }
+        }
+
+      }
+    }
+  )
+
 }
 
