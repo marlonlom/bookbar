3b0697d5f2e8756bfdf7394c30091aafec132c98
Marlon López <malm.gm@gmail.com>
2024-01-19 17:32:45 -0500

    feat(mobile-app): updated favorite books ui composables and viewmodel

--- apps/mobile-app/src/main/kotlin/dev/marlonlom/demos/bookbar/ui/features/books_favorite/FavoriteBooksRoute.kt
+++ apps/mobile-app/src/main/kotlin/dev/marlonlom/demos/bookbar/ui/features/books_favorite/FavoriteBooksRoute.kt
@@ -6,17 +6,23 @@
 package dev.marlonlom.demos.bookbar.ui.features.books_favorite
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dev.marlonlom.demos.bookbar.R
+import dev.marlonlom.demos.bookbar.domain.books.BooksListDomainItem
 import dev.marlonlom.demos.bookbar.ui.common.HeadlineTitle
 import dev.marlonlom.demos.bookbar.ui.common.WelcomeTitle
 import dev.marlonlom.demos.bookbar.ui.main.BookbarAppState
@@ -24,28 +30,76 @@ import dev.marlonlom.demos.bookbar.ui.main.BookbarAppState
 /**
  * Favorite books composable ui.
  *
- * @param appState Application ui state.
+ * @author marlonlom
+ *
+ * @param appState App ui state.
  * @param onBookItemClicked Action for Book item clicked.
  */
 @Composable
 fun FavoriteBooksRoute(
-  appState: BookbarAppState, onBookItemClicked: (String) -> Unit
+  appState: BookbarAppState,
+  onBookItemClicked: (String) -> Unit,
+  onRemoveFavoriteIconClicked: (String) -> Unit,
 ) {
-  val contentHorizontalPadding = if (appState.is10InTabletWidth) 80.dp else 20.dp
+  val contentHorizontalPadding = when {
+    appState.is10InTabletWidth -> 80.dp
+    appState.is7InTabletWidth -> 40.dp
+    else -> 20.dp
+  }
+
   Column(
     modifier = Modifier
       .fillMaxSize()
       .padding(horizontal = contentHorizontalPadding)
       .background(MaterialTheme.colorScheme.background),
-    horizontalAlignment = Alignment.CenterHorizontally,
+    horizontalAlignment = Alignment.CenterHorizontally
   ) {
-    WelcomeTitle(appState = appState)
-    HeadlineTitle(appState = appState, headlineTextRes = R.string.title_bottom_favorites)
-    LazyVerticalGrid(
-      columns = GridCells.Fixed(3),
-      content = {
+    when (appState.favoriteBooksList) {
+      FavoriteBooksUiState.Empty -> {
+        Text(text = "Empty results :(")
+      }
 
-      },
-    )
+      FavoriteBooksUiState.Loading -> {
+        Text(text = "Loading")
+      }
+
+      is FavoriteBooksUiState.Success -> {
+        LazyVerticalGrid(
+          columns = GridCells.Fixed(1),
+          horizontalArrangement = Arrangement.spacedBy(20.dp),
+          content = {
+            item(span = { GridItemSpan(maxLineSpan) }) {
+              WelcomeTitle(appState = appState)
+            }
+
+            item(span = { GridItemSpan(maxLineSpan) }) {
+              HeadlineTitle(
+                appState = appState,
+                headlineTextRes = R.string.title_bottom_favorites
+              )
+            }
+
+            val favoriteBooksCount = appState.favoriteBooksList.books.size
+            items(favoriteBooksCount) { bookIndex ->
+              val bookItem: BooksListDomainItem = appState.favoriteBooksList.books[bookIndex]
+              ClickableFavoriteBookItem(
+                bookIndex,
+                bookItem,
+                onBookItemClicked,
+                onRemoveFavoriteIconClicked
+              )
+              if ((bookIndex > 0).and(bookIndex < favoriteBooksCount)) {
+                Divider(
+                  modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp)
+                )
+              }
+            }
+          }
+        )
+      }
+    }
   }
 }
+
