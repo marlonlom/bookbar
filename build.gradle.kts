8ec2ffafc4d0b17e0d5de9e2ebca7eda95dcc534
Marlon López <malm.gm@gmail.com>
2024-03-01 13:33:49 -0500

    feat(mobile-app): Updated secrets properties fetching

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,5 +24,6 @@ plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.google.devtools.ksp) apply false
+  alias(libs.plugins.google.mapsplatform.secrets) apply false
   alias(libs.plugins.kotlin.android) apply false
 }
